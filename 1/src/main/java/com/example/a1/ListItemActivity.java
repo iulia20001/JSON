f853d59d90c09d832fab7ef9727bd701916806f8
iulia2999 <iuliasharina16@gmail.com>
2020-11-28 10:17:08 +0900

    JSON

---
+++ 1/src/main/java/com/example/a1/ListItemActivity.java
@@ -0,0 +1,138 @@
+package com.example.a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ListItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+
+        final ListView listView = (ListView)findViewById(R.id.ListView);
+
+        /***
+         * Sample JSON Code ***'
+         [{
+         "MemberID":"1",
+         "Name":"Елена",
+         "Tel":"4954876107"
+         },
+         {
+         "MemberID":"2",
+         "Name":"Сергей",
+         "Tel":"4954780121"
+         },
+         {
+         "MemberID":"3",
+         "Name":"Витюша",
+         "Tel":"4954543211"
+         }]
+         */
+
+        String rawJSON = "[{\"MemberID\":\"1\",\"Name\":\"Елена\",\"Tel\":\"4954876107\"}"
+                + ",{\"MemberID\":\"2\",\"Name\":\"Сергей\",\"Tel\":\"4954780121\"}"
+                + ",{\"MemberID\":\"3\",\"Name\":\"Витя\",\"Tel\":\"4954543211\"}]";
+
+        try {
+            JSONArray data = new JSONArray(rawJSON);
+
+            ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+            HashMap<String, String> map;
+
+            for (int i = 0; i < data.length(); i++)
+            {
+                JSONObject jsonObject = data.getJSONObject(i);
+
+                map = new HashMap<>();
+                map.put("MemberID", jsonObject.getString("MemberID"));
+                map.put("Name", jsonObject.getString("Name"));
+                map.put("Tel", jsonObject.getString("Tel"));
+
+                arrayList.add(map);
+            }
+            SimpleAdapter simpleAdapter;
+            simpleAdapter = new SimpleAdapter(this, arrayList,
+                    R.layout.list_item, new String[]{"MemberID",
+                    "Name", "Tel"}, new int[]{R.id.item_textViewMemberID,
+                    R.id.item_textViewName, R.id.item_textViewNumber});
+            listView.setAdapter(simpleAdapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JSONArray createJSON() throws JSONException {
+
+        ArrayList<JSONObject> jsonArrayList = new ArrayList<>();
+        JSONObject object;
+
+        /*** Ряд 1 ***/
+        object = new JSONObject();
+        object.put("MemberID", "1");
+        object.put("Name", "Анна");
+        object.put("Tel", "4954876107");
+        jsonArrayList.add(object);
+
+        /*** Ряд 2 ***/
+        object = new JSONObject();
+        object.put("MemberID", "2");
+        object.put("Name", "Николай");
+        object.put("Tel", "4954780121");
+        jsonArrayList.add(object);
+
+        /*** Ряд 3 ***/
+        object = new JSONObject();
+        object.put("MemberID", "3");
+        object.put("Name", "Сардана");
+        object.put("Tel", "4954543211");
+        jsonArrayList.add(object);
+
+        JSONArray jsonArray = new JSONArray(jsonArrayList);
+        return jsonArray;
+    }
+
+    public void onClick(View view) {
+        ListView listView = findViewById(R.id.ListView);
+
+        try {
+            JSONArray data = createJSON();
+
+            ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+            HashMap<String, String> hashMap;
+
+            for(int i = 0; i < data.length(); i++){
+                JSONObject jsonObject = data.getJSONObject(i);
+                hashMap = new HashMap<>();
+
+                hashMap.put("MemberID", jsonObject.getString("MemberID"));
+                hashMap.put("Name", jsonObject.getString("Name"));
+                hashMap.put("Tel", jsonObject.getString("Tel"));
+                arrayList.add(hashMap);
+            }
+
+            SimpleAdapter simpleAdapter;
+            simpleAdapter = new SimpleAdapter(this, arrayList,
+                    R.layout.list_item, new String[]{"MemberID",
+                    "Name", "Tel"}, new int[]{R.id.item_textViewMemberID,
+                    R.id.item_textViewName, R.id.item_textViewNumber});
+            listView.setAdapter(simpleAdapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
